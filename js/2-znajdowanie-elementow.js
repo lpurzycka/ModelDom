d12389a13aa07a0e9faaebe28169f6ec1913b51e
Lucyna Purzycka <ldamentka@gmail.com>
2018-01-13 10:23:33 +0100

    zakończono-znajdowanie elementow

---
+++ js/2-znajdowanie-elementow.js
@@ -0,0 +1,24 @@
+"use strict"
+//var parFirst = document.getElementById("parFirst");
+//console.log(parFirst);
+
+//var linki = document.getElementsByClassName("superlink");
+//console.log(linki);
+//
+//var linkiPoTagu = document.getElementsByTagName("a");
+//console.log(linkiPoTagu);
+
+var divPoId = document.querySelector("#parSecond");
+console.log(divPoId);
+
+//po id jak pobieramy, musimy wpisać #
+
+
+var superlinki =  document.querySelectorAll(".superlink");
+console.log(superlinki);
+
+//po classie pobieramy, musimy wpisać .
+
+
+
+
